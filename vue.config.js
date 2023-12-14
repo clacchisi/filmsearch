3e2678f18fadc1739de48612a7d4def184ed92ee
Claudio Landolfo <claudio.landolfo@bcame.it>
2023-12-14 17:11:00 +0100

    test change name

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,26 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     name: 'My first PWA', 
-    icons: [
-    {"src":"./assets/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
-    {"src":"./assets/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
-    {"src":"./assets/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
-    {"src":"./assets/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}]
+  //   icons: [
+  //   {"src":"./assets/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+  //   {"src":"./assets/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
+  //   {"src":"./assets/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
+  //   {"src":"./assets/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}
+  // ]
+  icons: [
+    {
+      src: "./assets/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+    {
+      src: "./assets/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+  ],
   }
 })
+
