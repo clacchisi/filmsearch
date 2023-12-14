efab2b3a61969c8a587559133b7a0dac882bb7e0
Claudio Landolfo <claudio.landolfo@bcame.it>
2023-12-14 17:16:49 +0100

    test change name

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,13 @@ module.exports = defineConfig({
     {
       src: "./assets/android-chrome-512x512.png",
       sizes: "512x512",
-      type: "image/png",
+      type: "png",
       purpose: "any maskable",
     },
     {
       src: "./assets/android-chrome-192x192.png",
       sizes: "192x192",
-      type: "image/png",
+      type: "png",
       purpose: "any maskable",
     },
   ],
