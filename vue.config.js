50c6a1f84f94a5f0009dacbf4d8c0eccb3a22d6c
Claudio Landolfo <claudio.landolfo@bcame.it>
2023-12-14 17:03:41 +0100

    test change name

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   pwa: {
     name: 'My first PWA', 
     icons: [
-    {"src":"./src/assets/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
-    {"src":"./src/assets/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
-    {"src":"./src/assets/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
-    {"src":"./src/assets/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}]
+    {"src":"./assets/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+    {"src":"./assets/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
+    {"src":"./assets/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
+    {"src":"./assets/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}]
   }
 })
