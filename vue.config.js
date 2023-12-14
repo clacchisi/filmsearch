1397dbe0664bb074aa384a4cd57b281f0186510b
Claudio Landolfo <claudio.landolfo@bcame.it>
2023-12-14 16:57:48 +0100

    test change name

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     name: 'My first PWA', 
+    icons: [
+    {"src":"./src/assets/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+    {"src":"./src/assets/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
+    {"src":"./src/assets/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
+    {"src":"./src/assets/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}]
   }
 })
